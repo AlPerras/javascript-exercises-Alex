cf561438d21a725897cd2bd4719af1e167af3257
Alexandre Perras <alexperras74@gmail.com>
2024-12-01 17:21:58 +0800

    finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+        return "ERROR";
+    }
+    let length = x + y;
+    let sum = 0;
+    if (x > y) {
+        for (let i = y; i < length; i++) {
+            sum += i;
+        }
+    } else if (y > x) {
+        for (let i = x; i < length; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
