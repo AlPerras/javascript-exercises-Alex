24d536ab4ec001bd8ac77b548ccaeaeae7ab43ac
Alexandre Perras <alexperras74@gmail.com>
2024-12-01 16:31:52 +0800

    finished 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const filteredArray = array.filter(element => !args.includes(element));
+    return filteredArray;
 };
 
 // Do not edit below this line
